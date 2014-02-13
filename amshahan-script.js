9a23e18235660aee88dca0332b05d56c7a38a067
Austin Michael Shahan <ubuntu>
2014-02-12 22:58:41 -0500

    Finished, Added DOM manipulation into error checking

--- amshahan-script.js
+++ amshahan-script.js
@@ -1,45 +1,89 @@
 function Validate() {
+	var errorString = "";
+
 	var firstName = $("#First").val();
 	
 	if (firstName.trim() == "" || firstName == null) {
-		alert("Empty First Name");
-		return false;
+		errorString += "Empty First Name<br />";
+		$("#First").css("background-color","#BB0000");
+		$("#First").css("color","#FFFFFF");
+	}
+	else {
+		$("#First").css("background-color","#FFFFFF");
+		$("#First").css("color","#000000");
 	}
 	
 	var lastName = $("#Last").val();
 	
 	if (lastName.trim() == "" || lastName == null) {
-		alert("Empty Last Name");
-		return false;
+		errorString += "Empty Last Name<br />";
+		$("#Last").css("background-color","#BB0000");
+		$("#Last").css("color","#FFFFFF");
+	}
+	else {
+		$("#Last").css("background-color","#FFFFFF");
+		$("#Last").css("color","#000000");
 	}
 	
 	var option = $("#Gender").val();
 	
 	if (option == "None") {
-		alert("Select a Gender");
-		return false;
+		errorString += "Gender Is Unselected<br />";
+		$("#Gender").css("background-color","#BB0000");
+		$("#Gender").css("color","#FFFFFF");
+	}
+	else {
+		$("#Gender").css("background-color","#FFFFFF");
+		$("#Gender").css("color","#000000");
 	}
 	
 	var email = $("#Email").val();
 	
 	if (email.trim() == "" || email == null || email.indexOf("@") < 1) {
-		alert("Enter a valid email address");
-		return false;
+		errorString += "Invalid Email Address<br />";
+		$("#Email").css("background-color","#BB0000");
+		$("#Email").css("color","#FFFFFF");
+	}
+	else {
+		$("#Email").css("background-color","#FFFFFF");
+		$("#Email").css("color","#000000");
 	}
 	
 	var password = $("#Password").val();
 	var repassword = $("#RePassword").val();
 	
 	if (password.trim() == "" || password == null) {
-		alert("Fill Out Password")
+		errorString += "Empty Password Field<br />";
+		$("#Password").css("background-color","#BB0000");
+		$("#Password").css("color","#FFFFFF");
+		$("#RePassword").css("background-color","#BB0000");
+		$("#RePassword").css("color","#FFFFFF");
+	}
+	else {
+		$("#Password").css("background-color","#FFFFFF");
+		$("#Password").css("color","#000000");
+		$("#RePassword").css("background-color","#FFFFFF");
+		$("#RePassword").css("color","#000000");
+	}
+	
+	if ((password.trim() != "" || password != null) && repassword != password) {
+		errorString += "Passwords Do Not Match<br />";
+		$("#RePassword").css("background-color","#BB0000");
+		$("#RePassword").css("color","#FFFFFF");
+	}
+	else if(!(password.trim() == "" || password == null)) {
+		$("#RePassword").css("background-color","#FFFFFF");
+		$("#RePassword").css("color","#000000");
+	}
+	
+	if (errorString != "" && errorString != null) {
+		errorString = "Errors:<br />" + errorString;
+		DisplayErrors(errorString);
 		return false;
 	}
 	
-	if (repassword != password) {
-		alert ("Passwords Do Not Match");
-		return false;
-	}
-	
-	alert("First : " + firstName + "\nLast : " + lastName + "\nOption : " + option + 
-	"\nEmail : " + email + "\nPassword : " + password + "\nRePassword : " + repassword);
+}
+
+function DisplayErrors(errorString) {
+	$("#ErrorContent").html(errorString);
 }
