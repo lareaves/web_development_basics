7f0ff641ff02cae8f39658e01c09a9b08a81a361
samerman <samerman@mix.wvu.edu>
2014-02-13 18:59:49 -0500

    fixed overwritten script
    I added back the default script.js and created my own samerman-script.js
    including the validateForm() function

--- samerman-script.js
+++ samerman-script.js
@@ -35,3 +35,19 @@ function test_counter() {
 	alert("counter passed test");
     }
 }
+function validateForm()
+{
+	var user = document.forms["form"]["username"].value;
+	var pass = document.forms["form"]["pass"].value;
+	var age = document.forms["form"]["age"].value;
+	if (user == null || user == "" || pass == null || pass == "")
+  	{
+  		alert("username must be filled out");
+  		return false;
+ 	}
+	if(age < 0 || age > 150)
+	{
+		alert("Age is out of bounds");
+		return false;
+	}
+}
